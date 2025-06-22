07a8f8637528fb927a735b9eb18742b9ad547ebe
ZZH-qwq <zhouzihan_mailbox@163.com>
2025-06-22 17:04:56 +0800

    chore: reorganize requirements into base and extras

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
 from setuptools import setup, find_packages
 
+base_requires = [
+    "loguru",
+    "numpy",
+    "nvidia-ml-py",
+    "pandas",
+    "psutil",
+    "schedule",
+]
+
+ai4s_requires = ["selenium", "openpyxl"]
+
+web_requires = [
+    "altair",
+    "pillow",
+    "plotly",
+    "streamlit",
+    "streamlit-autorefresh",
+    "streamlit-javascript",
+    "user-agents",
+]
+
 setup(
     name="contrail",
     version="0.1",
@@ -7,21 +28,9 @@ setup(
     entry_points={
         "console_scripts": ["contrail = contrail.cli:main"],
     },
-    install_requires=[
-        "altair",
-        "loguru",
-        "numpy",
-        "nvidia-ml-py",
-        "openpyxl",
-        "pandas",
-        "pillow",
-        "plotly",
-        "psutil",
-        "schedule",
-        "selenium",
-        "streamlit",
-        "streamlit-autorefresh",
-        "streamlit-javascript",
-        "user-agents",
-    ],
+    install_requires=base_requires,
+    extras_require={
+        "ai4s": ai4s_requires,
+        "web": web_requires,
+    },
 )
